ed3cfc79a3abd12ae679b4482886d900437033bc
kamalnadjieb <13514054@std.stei.itb.ac.id>
2016-11-17 15:14:57 +0700

    Update FFNN.java

--- awdenden/src/FFNN.java
+++ awdenden/src/FFNN.java
@@ -1,3 +1,11 @@
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+
+import java.io.File;
+import java.io.FileReader;
+
+import weka.core.converters.ConverterUtils.DataSource;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +17,15 @@
  * @author user
  */
 public class FFNN {
-    
+    public static void main(String args[]) throws Exception {
+        DataSource source = new DataSource("iris.arff");
+        Instances dataset = source.getDataSet();
+        
+        System.out.println(dataset.toSummaryString());
+        
+        ArffSaver saver = new ArffSaver();
+        saver.setInstances(dataset);
+        saver.setFile(new File("hasil.arff"));
+        saver.writeBatch();
+    }
 }
