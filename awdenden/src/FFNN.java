e655b8b231e128926bf96ed8b514c19a34eb3a6a
kamalnadjieb <13514054@std.stei.itb.ac.id>
2016-11-24 05:10:12 +0700

    Update FFNN

--- awdenden/src/FFNN.java
+++ awdenden/src/FFNN.java
@@ -9,10 +9,14 @@ public class FFNN extends AbstractClassifier {
     private int n_in;
     private int n_out;
     private int n_hidden;
-    private ArrayList<Weight> weights;
+    private final ArrayList<Weight> weights;
+    private final ArrayList<Double> nodes;
+    private final ArrayList<Double> errors;
     
     public FFNN()  {
         weights = new ArrayList<>();
+        nodes = new ArrayList<>();
+        errors = new ArrayList<>();
         //System.out.println("FFNN CTOR");
     }
     
@@ -54,203 +58,194 @@ public class FFNN extends AbstractClassifier {
         return this.n_hidden;
     }
     
-    public int getTotalN() {
+    public ArrayList<Weight> getWeights() {
+        return weights;
+    }
+    
+    public ArrayList<Double> getNodes() {
+        return nodes;
+    }
+    
+    public ArrayList<Double> getErrors() {
+        return errors;
+    }
+    
+    public int getNTotal() {
         return (this.n_in + this.n_out + this.n_hidden);
     }
     
+    public int getWTotal() {
+        if (this.n_hidden == 0) {
+            return (this.n_in * this.n_out);
+        } else {
+            return ((this.n_in * (this.n_hidden - 1)) + (this.n_hidden * this.n_out));
+        }
+    }
+    
     //
-    public double nettValue(int nodeOut, ArrayList<Double> node) {
+    public double nettValue(int nodeOut) {
+        //System.out.println("hai " + nodeOut);
         double result = 0.0;
-        int i = 0;
-        while (weights.get(i).getNodeOut() != nodeOut) {
-            ++i;
+        for (int i = 0; i < this.getWTotal(); ++i) {
+            //System.out.println("    halo " + i);
+            if (this.weights.get(i).getNodeOut() == nodeOut) {
+                result += this.nodes.get(this.weights.get(i).getNodeIn()) * this.weights.get(i).getValue();
+                //System.out.println(nodeOut);
+            }
         }
-        
-        while (weights.get(i).getNodeOut() == nodeOut) {
-            Weight w = weights.get(i);
-            result += node.get(w.getNodeIn()) * w.getValue();
-            ++i;
-        }
+        //System.out.println(result);
         return result;
     }
     
-    public double outputValue(double x) {
-        //System.out.println(1/(1+(Math.exp(-x))));
-        return 1/(1+(Math.exp((-1.0)*x)));
+    public double outputValue(double nett) {
+        return (1/(1+(Math.exp((-1.0)*nett))));
     }
     
     public double errorOutput(double output, double target) {
-        return output*(1-output)*(target-output);
+        return (output*(1-output)*(target-output));
     }
     
-    public double errorHidden(int nodeIn, double output, ArrayList<Double> error) {
+    public double errorHidden(int nodeIn, double output) {
         double result = 0.0;
-        int i = 0;
-        while (weights.get(i).getNodeIn() != nodeIn) {
-            ++i;
+        for (int i = 0; i < this.getWTotal(); ++i) {
+            if (this.weights.get(i).getNodeIn() == nodeIn) {
+                result += this.errors.get(this.weights.get(i).getNodeOut()) * this.weights.get(i).getValue();
+            }
         }
-        while ((i < this.weights.size()) && (weights.get(i).getNodeIn() == nodeIn)) {
-            Weight w = weights.get(i);
-            result += error.get(w.getNodeOut()) * w.getValue();
-            ++i;
-        }
-        double hasil = output*(1-output)* result;
-        //result = output*(1-output)* result;
-        return result;
+        return (output*(1-output)*result);
     }
     
-    public double getNewWeight(Weight w, ArrayList<Double> node, ArrayList<Double> error) {
-        //System.out.println(this.learning_rate);
-        return (w.getValue() + this.learning_rate * node.get(w.getNodeIn()) * error.get(w.getNodeOut()));
+    public double getNewWeight(int i) {
+        return (this.weights.get(i).getValue()
+                + this.learning_rate 
+                * this.nodes.get(this.weights.get(i).getNodeIn()) 
+                * this.errors.get(this.weights.get(i).getNodeOut()));
     }
     
     @Override
     public void buildClassifier(Instances dataset) throws Exception {
-        //Membuat dan inisialisasi weight
-        for (int i = 0; i < this.n_in; ++i) {
-            int batas_awal;
-            int batas_akhir;
-            if (this.n_hidden == 0) {
-                batas_awal = this.getNIn();
-                batas_akhir = this.getTotalN();
-            } else {
-                batas_awal = this.getNIn()+1;
-                batas_akhir = this.n_in + this.n_hidden;
-            }
-            for (int j = batas_awal; j < batas_akhir; ++j) {
-                Weight w = new Weight();
-                w.setValue(new Random().nextDouble() * 0.1 - 0.05);
-                w.setNodeIn(i);
-                w.setNodeOut(j);
-                //System.out.println(i + " " + j);
-                weights.add(w);    
-            }
-        }
+        /*Mengosongkan weights, nodes, dan errors jika belum kosong*/
+        this.weights.clear();
+        this.nodes.clear();
+        this.errors.clear();
         
-        if (this.n_hidden != 0) {
-            for (int i = this.n_in; i < this.n_in + this.n_hidden; ++i) {
-                for (int j = this.n_in + this.n_hidden; j < this.getTotalN(); ++j) {
-                    Weight w = new Weight();
-                    w.setValue(new Random().nextDouble() * 0.1 - 0.05);
-                    w.setNodeIn(i);
-                    w.setNodeOut(j);
-                    weights.add(w);
+        /*Inisialisasi weights*/
+        int counter = 0;
+        while (counter < this.getWTotal()) {
+            for (int j = 0; j < (this.getNTotal() - this.n_out); ++j) { //index node_in
+                int batas_awal;
+                int batas_akhir;
+                if ((j < this.n_in) && (this.n_hidden != 0)) {
+                    batas_awal = this.n_in + 1;
+                    batas_akhir = this.n_in + this.n_hidden;
+                } else {
+                    batas_awal = this.n_in + this.n_hidden;
+                    batas_akhir = this.getNTotal();
+                }
+                for (int k = batas_awal; k < batas_akhir; ++k) { //index node_out
+                    this.weights.add(new Weight());
+                    this.weights.get(counter).setValue(new Random().nextDouble() * 0.1 - 0.05);
+                    this.weights.get(counter).setNodeIn(j);
+                    this.weights.get(counter).setNodeOut(k);
+                    ++counter;
                 }
             }
         }
         
-        //
+        /*Inisialisasi nodes dan errors*/
+        for (int i = 0; i < this.getNTotal(); ++i) {
+            this.nodes.add(1.0);
+            this.errors.add(0.0);
+        }
+        
         int loop = 0;
-        while (loop < 10000) {
-            for (int i = 0; i < dataset.numInstances(); ++i) {
-                //Membuat node
-                ArrayList<Double> node;
-                node = new ArrayList<>();
-            
-                //-->Forward
-                for (int j = 0; j < this.getTotalN(); ++j) {
-                    double result;
-                    if (j < this.n_in) {
-                        if (j == 0) {
-                            result = 1.0;
-                        } else {
-                            result = dataset.instance(i).value(j-1);
-                        }
-                    } else {
-                        if ((j == this.n_in) && (this.n_hidden != 0)) {
-                            result = 1.0;
-                        } else {
-                            result = this.outputValue(this.nettValue(j, node));
-                        }
+        while (loop < 1) {
+            for (int i = 0; i < dataset.numInstances(); ++i) { //index dataset
+                /*Inisialisasi node input*/
+                for (int j = 1; j < this.n_in; ++j) { //index input
+                    this.nodes.set(j, dataset.instance(i).value(j-1));
+                }
+                
+                //-->
+                for (int j = this.n_in; j < this.getNTotal(); ++j) { //index output
+                    if (!((j == this.n_in) && (this.n_hidden != 0))) {
+                        this.nodes.set(j, this.outputValue(this.nettValue(j)));
                     }
-                    node.add(result);
+                    //System.out.println(nodes.get(j));
                 }
                 
-                //<--Backward
-                ArrayList<Double> error;
-                error = new ArrayList<>();
-                for (int j = 0; j < this.getTotalN(); ++j) {
-                    error.add(0.00);
-                }
-                
-                //Target
+                //<--
                 double target = dataset.instance(i).classValue();
-                
-                //Output Error
-                double temp = this.n_out-1;
-                
-                for (int j = this.getTotalN()-1; j >= this.n_in + this.n_hidden; --j) {
-                    if (temp == target) {
-                        error.set(j, errorOutput(node.get(j), 1.0));
-                    } else {
-                        error.set(j, errorOutput(node.get(j), 0.0));
-                    }
-                    --temp;
-                }
-            
-                if (this.n_hidden != 0) {
-                    for (int j = this.n_in + this.n_hidden - 1; j >= this.n_in; --j) {
-                        if (j == this.n_in) {
-                            error.set(j, 1.0);
+                for (int j = this.getNTotal()-1; j >= this.n_in; --j) {
+                    if ((j == this.n_in) && (this.n_hidden != 0)) {
+                        //Do Nothing
+                    } else if (j >= this.getNTotal() - this.n_out) { //errorOutput
+                        double output = j - this.getNTotal() + this.n_out;
+                        if (output == target) {
+                            this.errors.set(j, this.errorOutput(j, 1.0));
                         } else {
-                            error.set(j, this.errorHidden(j, node.get(j), error));
+                            this.errors.set(j, this.errorOutput(j, 0.0));
                         }
-                    }
+                    } else { //errorHidden
+                        //System.out.println(j);
+                        this.errors.set(j, this.errorHidden(j, this.nodes.get(j)));
+                    } 
                 }
-            
+                
                 //Set nilai baru pada weights
-                for (int j = 0; j < weights.size(); ++j) {
-                    weights.get(j).setValue(this.getNewWeight(weights.get(j), node, error));
+                for (int j = 0; j < this.getWTotal(); ++j) {
+                    weights.get(j).setValue(this.getNewWeight(j));
                 }
+                /*
+                System.out.println("begin");
+                System.out.println(nodes.get(10));
+                System.out.println(nodes.get(11));
+                System.out.println(nodes.get(12));
+                System.out.println("end");
+                */
             }
-        ++loop;
+            ++loop;
         }
+        this.nodes.clear();
+        this.errors.clear();
     }
     
     @Override
     public double classifyInstance(Instance data) throws Exception {
-        //Membuat node
-        ArrayList<Double> node;
-        node = new ArrayList<>();
+        this.nodes.clear();
         
-        //-->Forward
-        for (int j = 0; j < this.getTotalN(); ++j) {
-            double result;
-            if (j < this.n_in) {
-                if (j == 0) {
-                    result = 1.0;
-                } else {
-                    result = data.value(j-1);
-                }
-            } else {
-                if ((j == this.n_in) && (this.n_hidden != 0)) {
-                    result = 1.0;
-                } else {
-                    result = this.outputValue(this.nettValue(j, node));
-                }
-            }
-            node.add(result);
+        /*Inisialisasi nodes dan errors*/
+        for (int i = 0; i < this.getNTotal(); ++i) {
+            this.nodes.add(1.0);
+        }
+        
+        for (int j = this.n_in; j < this.getNTotal(); ++j) { //index output
+            if (!((j == this.n_in) && (this.n_hidden != 0))) {
+                this.nodes.set(j, this.outputValue(this.nettValue(j)));
+            }    
         }
         
         double result = 0.0;
         if (data.numClasses() > 2) {
             double counter = 0.0;
             double result_class = 0.0;
-            double result_output = node.get(this.n_in + this.n_hidden);
-            for (int k = this.n_in + this.n_hidden + 1; k < this.getTotalN(); ++k) {
+            double result_output = nodes.get(this.n_in + this.n_hidden);
+            for (int k = this.n_in + this.n_hidden + 1; k < this.getNTotal(); ++k) {
                 ++counter;
-                if (result_output < node.get(k)) {
-                    result_output = node.get(k);
-                    result_class = counter;
+                if (result_output < nodes.get(k)) {
+                    result_output = nodes.get(k);
+                    result = counter;
                 }
             }
-            return result_class;
         } else {
-            if (node.get(node.size()-1) > 0.5) {
-                return 0.0;
+            if (nodes.get(nodes.size()-1) > 0.5) {
+                result = 0.0;
             } else {
-                return 1.0;
+                result = 1.0;
             }
         }
+        
+        this.nodes.clear();
+        return result;
     }
 }
