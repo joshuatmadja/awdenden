8ceac09e527e3e1ae2858115b493051aa9f36045
kamalnadjieb <13514054@std.stei.itb.ac.id>
2016-11-24 22:01:40 +0700

    Update FFNN

--- awdenden/src/FFNN.java
+++ awdenden/src/FFNN.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Random;
 import weka.classifiers.AbstractClassifier;
+import weka.core.Capabilities;
+import weka.core.CapabilitiesHandler;
 import weka.core.Instance;
 import weka.core.Instances;
 
@@ -17,7 +19,6 @@ public class FFNN extends AbstractClassifier {
         weights = new ArrayList<>();
         nodes = new ArrayList<>();
         errors = new ArrayList<>();
-        //System.out.println("FFNN CTOR");
     }
     
     //SETTER
@@ -84,19 +85,12 @@ public class FFNN extends AbstractClassifier {
     
     //
     public double nettValue(int nodeOut) {
-        //System.out.println("hai " + nodeOut);
         double result = 0.0;
         for (int i = 0; i < this.getWTotal(); ++i) {
             if (this.weights.get(i).getNodeOut() == nodeOut) {
-                /*System.out.println("---");
-                System.out.println(this.weights.get(i).getValue());
-                System.out.println("+++");
-                */
-                result += this.nodes.get(this.weights.get(i).getNodeIn()) * this.weights.get(i).getValue();
-                //System.out.println(nodeOut);
+                result += (this.nodes.get(this.weights.get(i).getNodeIn()) * this.weights.get(i).getValue());
             }
         }
-        //System.out.println(result);
         return result;
     }
     
@@ -105,24 +99,22 @@ public class FFNN extends AbstractClassifier {
     }
     
     public double errorOutput(double output, double target) {
-        return (output*(1-output)*(target-output));
+        return (output * (1.0 - output) * (target - output));
     }
     
-    public double errorHidden(int nodeIn, double output) {
-        double result = 0.0;
+    public double errorHidden(int nodeIn) {
+        double sigma = 0.0;
+        double output = this.nodes.get(nodeIn);
         for (int i = 0; i < this.getWTotal(); ++i) {
             if (this.weights.get(i).getNodeIn() == nodeIn) {
-                result += this.errors.get(this.weights.get(i).getNodeOut()) * this.weights.get(i).getValue();
+                sigma += (this.weights.get(i).getValue() * this.errors.get(this.weights.get(i).getNodeOut()));
             }
         }
-        return (output*(1-output)*result);
+        return (output * (1.0 - output) * sigma);
     }
     
     public double getNewWeight(int i) {
-        return (this.weights.get(i).getValue()
-                + this.learning_rate 
-                * this.nodes.get(this.weights.get(i).getNodeIn()) 
-                * this.errors.get(this.weights.get(i).getNodeOut()));
+        return (this.weights.get(i).getValue() + (this.learning_rate * this.nodes.get(this.weights.get(i).getNodeIn()) * this.errors.get(this.weights.get(i).getNodeOut())));
     }
     
     @Override
@@ -161,54 +153,70 @@ public class FFNN extends AbstractClassifier {
             this.errors.add(0.0);
         }
         
+        ArrayList<Double> temp1 = new ArrayList<>();
+        ArrayList<Double> temp2 = new ArrayList<>();
         int loop = 0;
-        while (loop < 1) {
+        boolean isStop = false;
+        while (!isStop) {
             for (int i = 0; i < dataset.numInstances(); ++i) { //index dataset
-                /*Inisialisasi node input*/
+                //-->Mengisi node input
                 for (int j = 1; j < this.n_in; ++j) { //index input
                     this.nodes.set(j, dataset.instance(i).value(j-1));
                 }
                 
-                //-->Mendapatkan nilai output
+                //-->Mengisi node hidden dan output
                 for (int j = this.n_in; j < this.getNTotal(); ++j) { //index output
                     if (!((j == this.n_in) && (this.n_hidden != 0))) {
                         this.nodes.set(j, this.outputValue(this.nettValue(j)));
                     }
-                    //System.out.println(nodes.get(j));
                 }
                 
-                //<--Mendapatkan nilai error
+                //<--Mengisi nilai error
                 double target = dataset.instance(i).classValue();
-                for (int j = this.getNTotal()-1; j >= this.n_in; --j) {
+                for (int j = this.getNTotal() - 1; j >= this.n_in; --j) {
                     if ((j == this.n_in) && (this.n_hidden != 0)) {
                         //Do Nothing
                     } else if (j >= this.getNTotal() - this.n_out) { //errorOutput
-                        double output = j - this.getNTotal() + this.n_out;
-                        if (output == target) {
-                            this.errors.set(j, this.errorOutput(j, 1.0));
+                        if (this.n_out == 1) {
+                            if (this.nodes.get(j) >= 0.5) {
+                                this.errors.set(j, this.errorOutput(this.nodes.get(j), 1.0));
+                            } else {
+                                this.errors.set(j, this.errorOutput(this.nodes.get(j), 0.0));
+                            }
                         } else {
-                            this.errors.set(j, this.errorOutput(j, 0.0));
+                            double output = j - this.getNTotal() + this.n_out;
+                            if (output == target) {
+                                this.errors.set(j, this.errorOutput(this.nodes.get(j), 1.0));
+                            } else {
+                                this.errors.set(j, this.errorOutput(this.nodes.get(j), 0.0));
+                            }
                         }
                     } else { //errorHidden
-                        //System.out.println(j);
-                        this.errors.set(j, this.errorHidden(j, this.nodes.get(j)));
+                        this.errors.set(j, this.errorHidden(j));
                     }
-                    //System.out.println(this.errors.get(j));
                 }
-                //System.out.println(this.errors.get(1));
                 
-                //Set nilai baru pada weights
+                //-->Mengisi nilai weights yang baru
                 for (int j = 0; j < this.getWTotal(); ++j) {
                     this.weights.get(j).setValue(this.getNewWeight(j));
                 }
-                //System.out.println(weights.get(34).getValue());
-                /*
-                System.out.println("begin");
-                System.out.println(nodes.get(10));
-                System.out.println(nodes.get(11));
-                System.out.println(nodes.get(12));
-                System.out.println("end");
-                */
+            }
+            if (loop == 0) {
+                for (int j = this.getNTotal() - this.n_out; j < this.getNTotal(); ++j) {
+                    temp1.add(this.nodes.get(j));
+                }
+            } else {
+                isStop = true;
+                for (int j = this.getNTotal() - this.n_out; j < this.getNTotal(); ++j) {
+                    temp2.add(this.nodes.get(j));
+                }
+                for (int j = 0; j < this.n_out; ++j) {
+                    if ((temp1.get(j) - temp2.get(j) > 0.000005) || (temp1.get(j) - temp2.get(j) < -0.000005)) {
+                        isStop = false;
+                    }
+                    temp1.set(j, temp2.get(j));
+                }
+                temp2.clear();
             }
             ++loop;
         }
@@ -218,6 +226,32 @@ public class FFNN extends AbstractClassifier {
     
     @Override
     public double classifyInstance(Instance data) throws Exception {
+        double result = 0.0;
+        double[] results = this.distributionForInstance(data);
+        double max = -1e9;
+        
+        if (this.n_out == 1) {
+            if (results[0] >= 0.5) {
+                result = 0.0;
+            } else {
+                result = 1.0;
+            }
+        } else {
+            for (int i = 0; i < this.n_out; ++i) {
+                if (results[i] > max) {
+                    result = (double) i;
+                    max = results[i];
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    @Override
+    public double[] distributionForInstance(Instance instnc) throws Exception {
+        double[] result = new double[this.n_out];
+        
         /*Mengosongkan nodes*/
         this.nodes.clear();
         
@@ -226,34 +260,32 @@ public class FFNN extends AbstractClassifier {
             this.nodes.add(1.0);
         }
         
-        //-->
+        //-->Mengisi node input
+        for (int j = 1; j < this.n_in; ++j) { //index input
+            this.nodes.set(j, instnc.value(j-1));
+        }
+                
+        //-->Mengisi node hidden dan output
         for (int j = this.n_in; j < this.getNTotal(); ++j) { //index output
             if (!((j == this.n_in) && (this.n_hidden != 0))) {
                 this.nodes.set(j, this.outputValue(this.nettValue(j)));
             }
         }
         
-        double result = 0.0;
-        if (data.numClasses() > 2) {
-            double counter = 0.0;
-            double result_class = 0.0;
-            double result_output = nodes.get(this.n_in + this.n_hidden);
-            for (int k = this.n_in + this.n_hidden + 1; k < this.getNTotal(); ++k) {
-                ++counter;
-                if (result_output < nodes.get(k)) {
-                    result_output = nodes.get(k);
-                    result = counter;
-                }
-            }
-        } else {
-            if (nodes.get(nodes.size()-1) > 0.5) {
-                result = 0.0;
-            } else {
-                result = 1.0;
-            }
+        int counter = 0;
+        for (int i = this.n_in + this.n_hidden; i < this.getNTotal(); ++i) {
+            result[counter] = this.nodes.get(i);
+            ++counter;
         }
         
         this.nodes.clear();
+        return result;
+    }
+    
+    @Override
+    public Capabilities getCapabilities() {
+        Capabilities result = new Capabilities((CapabilitiesHandler) this);
+        result.enableAll();
         return result;
     }
 }
--- awdenden/src/Main.java
+++ awdenden/src/Main.java
@@ -6,13 +6,18 @@ import java.util.Random;
 
 import weka.core.Instances;
 import weka.filters.Filter;
-import weka.filters.unsupervised.attribute.NominalToBinary;
 import weka.classifiers.Evaluation;
+import weka.filters.unsupervised.attribute.Normalize;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) throws Exception {
         /*LOAD DATA TRAIN*/
-        FileReader train_reader = new FileReader("C:\\Program Files\\Weka-3-8\\data\\iris.arff");
+        System.out.print("Masukkan direktori menuju file .arff : ");
+        Scanner cin = new Scanner(System.in);
+        String filepath = cin.nextLine();
+        FileReader train_reader = new FileReader(filepath);
+        //FileReader train_reader = new FileReader("C:\\Program Files\\Weka-3-8\\data\\iris.arff");
         //FileReader train_reader = new FileReader("C:\\Users\\Kamal Nadjieb\\Documents\\ITB\\AKADEMIK\\V\\IF3170\\Tugas Besar 2\\mush.arff");
         //FileReader train_reader = new FileReader("C:\\Users\\Kamal Nadjieb\\Documents\\ITB\\AKADEMIK\\V\\IF3170\\Tugas Besar 2\\Team_test.arff");
         Instances data_train = new Instances(train_reader);
@@ -20,17 +25,8 @@ public class Main {
             data_train.setClassIndex(data_train.numAttributes() - 1);
         }
         
-        //System.out.println(data_train.instance(0).classValue());
-        //System.out.println(data_train.numInstances());
-        
-        /*NOMINAL TO BINARY ATTRIBUTE*/
-        //set options
-        String[] options = new String[2];
-        options[0] = "-R";
-        options[1] = "first-last";
-        //Apply discretization
-        NominalToBinary norm = new NominalToBinary();
-        norm.setOptions(options);
+        /*NORMALIZE ATTRIBUTE*/
+        Normalize norm = new Normalize();
         norm.setInputFormat(data_train);
         Instances data_train_new = Filter.useFilter(data_train, norm);
         //data_train_new.randomize(new Random(1));
@@ -46,7 +42,9 @@ public class Main {
         ffnn.setLearningRate(0.05);
         ffnn.setNIn(n_in);
         ffnn.setNOut(n_out);
-        ffnn.setNHidden(4);
+        System.out.print("Masukkan jumlah hidden node yang diinginkan: ");
+        int n_hidden = cin.nextInt();
+        ffnn.setNHidden(n_hidden);
         ffnn.buildClassifier(data_train_new);
         
         /* EVALUATION */
@@ -54,15 +52,15 @@ public class Main {
         Evaluation eval = new Evaluation(data_train_new);
         eval.evaluateModel(ffnn, data_train_new);
         //eval.crossValidateModel(ffnn, data_train_new, 10, new Random(1));
-        System.out.println(eval.errorRate()); //Printing Training Mean root squared Error
         System.out.println(eval.toSummaryString()); //Summary of Training
-        System.out.println(eval.toMatrixString());
+        System.out.println(eval.toMatrixString()); //Confusion Matrix
         //*/
         
         /*CLASSIFY DATAPREDICT*/
         ///*
         Instances datapredict = new Instances(
-        new BufferedReader(new FileReader("C:\\Program Files\\Weka-3-8\\data\\iris.arff")));
+        new BufferedReader(new FileReader(filepath)));
+        //new BufferedReader(new FileReader("C:\\Program Files\\Weka-3-8\\data\\iris.arff")));
         //new BufferedReader(new FileReader("C:\\Users\\Kamal Nadjieb\\Documents\\ITB\\AKADEMIK\\V\\IF3170\\Tugas Besar 2\\mush.arff")));
         //new BufferedReader(new FileReader("C:\\Users\\Kamal Nadjieb\\Documents\\ITB\\AKADEMIK\\V\\IF3170\\Tugas Besar 2\\Team_test.arff")));
         datapredict.setClassIndex(datapredict.numAttributes() - 1);
